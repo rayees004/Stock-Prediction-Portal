1684036fab3faa2e53bbe7d0768d7286bea780f9
rayees004 <mrc53445@gmail.com>
2026-06-01 12:12:11 +0530

    no changes applaid

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -139,4 +139,5 @@ REST_FRAMEWORK = {
 }
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),}
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    }
